3c0c36907a7b159e8c84e3305dab770ce2cec350
cunhaDev <dev.mateuscunha@gmail.com>
2021-06-12 15:29:41 -0400

    feat: resolvendo exercicio multiplo de 3 com Modulo

---
+++ sintaxe-variavel-e-fluxo/src/aula03/ExercicioModulo.java
@@ -0,0 +1,12 @@
+package aula03;
+
+public class ExercicioModulo {
+	public static void main(String[] args) {
+		for(int multiplo = 1; multiplo <= 100; multiplo++) {
+			if(multiplo % 3 == 0)
+			{
+				System.out.println(multiplo);
+			}
+		}
+	}
+}

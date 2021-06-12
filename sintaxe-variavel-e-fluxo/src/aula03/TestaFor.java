33d6dae9992418be331bf79e294da69275c29941
cunhaDev <dev.mateuscunha@gmail.com>
2021-06-12 15:01:34 -0400

    feat: trabalhadno com For

---
+++ sintaxe-variavel-e-fluxo/src/aula03/TestaFor.java
@@ -0,0 +1,10 @@
+package aula03;
+
+public class TestaFor {
+	public static void main(String[] args) {
+		for (int contador = 0; contador <= 10; contador++) {
+			System.out.println(contador);
+
+		}
+	}
+}

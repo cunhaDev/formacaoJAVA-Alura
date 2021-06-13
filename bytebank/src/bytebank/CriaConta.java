7c8a4a8210bc270d571caba43d3763b54e00b1a3
cunhaDev <dev.mateuscunha@gmail.com>
2021-06-13 15:29:45 -0400

    feat: Segunda Instância

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -4,7 +4,12 @@ public class CriaConta {
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
 		primeiraConta.saldo = 200;
-		
 		System.out.println(primeiraConta.saldo);
+		primeiraConta.saldo += 100;
+		System.out.println("Priemira Conta tem: " + primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		System.out.println("Segunda Conta tem: " + segundaConta.saldo);
 	}
 }

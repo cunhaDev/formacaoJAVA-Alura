f26e83df5c5a2d7de183eb3135fe946c96cf7900
cunhaDev <dev.mateuscunha@gmail.com>
2021-06-16 19:04:35 -0400

    feat: Getters e Setters de referência

--- bytebank-composto/src/TelaBanco.java
+++ bytebank-composto/src/TelaBanco.java
@@ -2,9 +2,9 @@
 public class TelaBanco {
 	public static void main(String[] args) {
 		Cliente paulo = new Cliente();
-		paulo.nome = "Paulo Silveira";
-		paulo.cpf = "222.222.222.02";
-		paulo.profissao = "Programador Java";
+		paulo.setNome("Paulo Silveira");
+		paulo.setCpf("222.222.222.02");
+		paulo.setProfissao("Programador Java");
 		
 		Conta contaPaulo = new Conta();
 		contaPaulo.depositar(200);

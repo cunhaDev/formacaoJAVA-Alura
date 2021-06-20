af6934ce59858d314ddf2c9e0c1c0e9d05fe027b
cunhaDev <dev.mateuscunha@gmail.com>
2021-06-20 11:19:56 -0400

    feat: Começando com a herança

---
+++ bytebank-herdado/src/TesteGerente.java
@@ -0,0 +1,17 @@
+
+public class TesteGerente {
+	public static void main(String[] args) {
+		Gerente g1 = new Gerente();
+		g1.setNome("Nic Gerente");
+		g1.setCpf("0006546352411-52");
+		g1.setSalario(10000.00);
+		
+		System.out.println(g1.getNome());
+		System.out.println(g1.getCpf());
+		System.out.println(g1.getSalario());
+		
+		g1.setSenha(1111);
+		boolean autenticou = g1.autentica(1111);
+		System.out.println(autenticou);
+	}
+}

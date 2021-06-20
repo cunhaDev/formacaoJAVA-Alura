b9d830e06a61b94e4b1632d688aab41b5c451b5d
cunhaDev <dev.mateuscunha@gmail.com>
2021-06-20 11:03:20 -0400

    feat: Tipos de funcionários

---
+++ bytebank-herdado/src/FuncionarioTeste.java
@@ -0,0 +1,51 @@
+
+public class FuncionarioTeste {
+
+	private String nome;
+	private String cpf;
+	private double salario;
+	private int tipo = 0; // 0 = funcionario, 1 = gerente
+	
+	public double getBonificacao() {
+		if(this.tipo == 0) { // funcionario
+			return this.salario  * 0.1;
+		} else if(this.tipo == 1) { // gerente
+			return this.salario;
+		} else { // diretor
+			return this.salario + 1000;
+		}
+	}
+	
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	
+	public int getTipo() {
+		return tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+}

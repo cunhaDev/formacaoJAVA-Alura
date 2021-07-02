5eecde34fa5a682384b2ca6d0a34a7736347dc6e
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-02 09:16:26 -0400

    feat: herança e classes abstratas

--- bytebank-herdado/src/Administrador.java
+++ bytebank-herdado/src/Administrador.java
@@ -1,5 +1,5 @@
 
-public class Gerente extends FuncionarioAutenticavel{// extends = herança/herdar
+public class Administrador extends FuncionarioAutenticavel {// extends = herança/herdar
 
 	public double getBonificacao() {// reescrita de metodo: mesma assinatura da classe super
 		return getSalario(); // super: referencia que a variavel ou metodo que vem da super classe ou Classe Mae

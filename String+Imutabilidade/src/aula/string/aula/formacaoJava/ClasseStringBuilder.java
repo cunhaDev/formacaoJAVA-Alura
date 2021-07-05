3a4f9e92ca6a3c709edfea487f77827417795bd6
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-05 17:08:00 -0400

    feat: String e seus metodos + StringBuilder

---
+++ String+Imutabilidade/src/aula/string/aula/formacaoJava/ClasseStringBuilder.java
@@ -0,0 +1,31 @@
+package aula.string.aula.formacaoJava;
+
+public class ClasseStringBuilder {
+	public static void main(String[] args) {
+
+		//forma antiga
+/*		String texto = "Socorram";
+		texto = texto.concat("-");
+		texto = texto.concat("me");
+		texto = texto.concat(", ");
+		texto = texto.concat("subi ");
+		texto = texto.concat("no ");
+		texto = texto.concat("ônibus ");
+		texto = texto.concat("em ");
+		texto = texto.concat("Marrocos");
+		System.out.println(texto);  */
+		
+		// nova forma
+		StringBuilder builder = new StringBuilder("Socorram");
+		builder.append("-");
+		builder.append("me");
+		builder.append(", ");
+		builder.append("subi ");
+		builder.append("no ");
+		builder.append("ônibus ");
+		builder.append("em ");
+		builder.append("Marrocos");
+		String texto = builder.toString();
+		System.out.println(texto);
+	}
+}
---
+++ String+Imutabilidade/src/aula/string/aula/formacaoJava/MetodosString.java
@@ -0,0 +1,47 @@
+package aula.string.aula.formacaoJava;
+
+public class MetodosString {
+	public static void main(String[] args) {
+
+		
+		/**
+		 * replace : substitui o primeiro parametro pelo segundo
+		 */
+//		String teste = "Alura";
+//		teste.replace(" ", teste);
+		
+		/**
+		 * charArt : pega um caracter de uma determinada posição
+		 */
+//		String teste = "Alura";
+//		char a = teste.charAt(4);
+//		System.out.println(a);
+		
+		/**
+		 * indexOf : devolve a posição da string passada no parametro
+		 */
+//		String teste = "Alura";
+//		int pos = teste.indexOf("ur");
+//		System.out.println(pos);
+		
+		/**
+		 * substring : devolve a string a partir daquela posição
+		 */
+//		String teste = "Alura";
+//		String sub = teste.substring(1);
+//		System.out.println(sub);
+		
+		/**
+		 * length : devolve o tamanho da string( quantidade de caracteres)
+		 */
+//		String teste = "Alura";
+//		System.out.println(teste.length());
+		
+		/**
+		 * isEmpty : devolve um boolean dizendo se esta ou nao vazio
+		 */
+//		String vazio = "a";
+//		System.out.println(vazio.isEmpty());
+		
+	}
+}

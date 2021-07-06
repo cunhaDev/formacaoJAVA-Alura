157f843c3b5dda520ac1a7df7056182d0cda7cec
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-06 15:46:40 -0400

    feat: List

---
+++ String+Imutabilidade/src/aula/collectiona/formacaoJava/arrays/ArrayListEquals.java
@@ -0,0 +1,17 @@
+package aula.collectiona.formacaoJava.arrays;
+
+import java.util.ArrayList;
+
+public class ArrayListEquals {
+	public static void main(String[] args) {
+		
+		ArrayList<String> lista = new ArrayList<>();
+		
+		lista.add("Objeto 1");
+		lista.add("Objeto 1");
+		
+		// contains verifica se ja existe o objeto na lista
+		boolean existe = lista.contains("Objeto 1");
+		System.out.println("Existe? " + existe);
+	}
+}
---
+++ String+Imutabilidade/src/aula/collectiona/formacaoJava/arrays/List.java
@@ -0,0 +1,9 @@
+package aula.collectiona.formacaoJava.arrays;
+
+import java.util.Arrays;
+
+public class List {
+	public static void main(String[] args) {
+		java.util.List<String> argumentos = Arrays.asList(args);
+	}
+}

a840905da8f9a8bf770147edc57b6932ff9e9d7b
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-06 12:41:00 -0400

    feat: Array com Object

---
+++ String+Imutabilidade/src/aula/collectiona/formacaoJava/ArrayObject.java
@@ -0,0 +1,13 @@
+package aula.collectiona.formacaoJava;
+
+public class ArrayObject {
+	public static void main(String[] args) {
+		// Array do tipo Object > objeto literal que suporta qualquer coisa
+		Object[] obj = new Object[5];
+		obj[0] = "String";
+		obj[1] = 1324;
+		obj[2] = true;
+		obj[3] = 'c';
+		obj[4] = 15246541687465465L;
+	}
+}

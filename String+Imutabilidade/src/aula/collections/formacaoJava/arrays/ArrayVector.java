eeee84c299a626032e890a475985ccf588ef2cde
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-07 08:38:01 -0400

    feat: threadsafe

---
+++ String+Imutabilidade/src/aula/collections/formacaoJava/arrays/ArrayVector.java
@@ -0,0 +1,10 @@
+package aula.collections.formacaoJava.arrays;
+
+import java.util.List;
+import java.util.Vector;
+
+public class ArrayVector {
+	
+	List<String> vector = new Vector<String>(); //Thread Safe quando precisa executar Varias pilhas
+
+}

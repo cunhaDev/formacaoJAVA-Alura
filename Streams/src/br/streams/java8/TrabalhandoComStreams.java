63605d8ee521cc04fea32c2d505e3d0c128413fb
cunhaDev <dev.mateuscunha@gmail.com>
2021-07-09 16:19:39 -0400

    Streams: operacoes Intermediarias

--- Streams/src/br/streams/java8/TrabalhandoComStreams.java
+++ Streams/src/br/streams/java8/TrabalhandoComStreams.java
@@ -8,12 +8,25 @@ public class TrabalhandoComStreams {
 		
 		listaDePessoas.add("Mateus");
 		listaDePessoas.add("Maria");
+		listaDePessoas.add("Maria");
+		listaDePessoas.add("Joao");
 		listaDePessoas.add("Joao");
 		listaDePessoas.add("Filipy");
 		listaDePessoas.add("Carol");
 		
-		listaDePessoas.stream().forEach(p -> {
-			System.out.println(p);
+		listaDePessoas.stream()
+		.skip(1)//ignora primeiro nome
+		.limit(2)//limita a lista a 2 nomes
+		.distinct() //tira tudo que for igual e deixa so 
+		.forEach(p -> {
+			System.out.println(p);// tem que exibir Maria, joao
 		});
+		
+		/** operações Intermediarias
+		 * skip: pula a quantidade de dados e segue o fluxo
+		 * limit: limita a coleção a uma certa quantidade
+		 * distinct: nao deixa exibir elementos repetidos, usa equals e hashcode pra fazer a comparação
+		 * filter: filtra algo da lista
+		 */
 	}
 }
